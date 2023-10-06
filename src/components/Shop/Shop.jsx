6d9f28ae786a16525eaac109304a11076b1e6eed
tasfiq512 <tasfiq512@gmail.com>
2023-10-06 16:29:03 +0600

    fetch

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    const[products,setProducts]= useState([]);
+    useEffect(() =>{
+          fetch( 'products.json')
+          .then(res => res.json())
+          .then(data => setProducts(data))
+    },[])
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h2>Products coming here {products.length}</h2>
+            </div>
+            <div className='card-container'>
+                <h2>Order summary</h2>
+            </div>
+
+        </div>
+    );
+};
+
+export default Shop;
